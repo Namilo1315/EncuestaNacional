ce7e27635e38aa2301e687c60f3daa7183a9546a
nalopez <natalia89751315@gmail.com>
2023-10-17 21:30:40 -0300

    Encuesta

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function calcularVoto() {
+    const respuestas = {
+        p1: document.querySelector('input[name="p1"]:checked').value,
+        p2: document.querySelector('input[name="p2"]:checked').value,
+        p3: document.querySelector('input[name="p3"]:checked').value,
+        p4: document.querySelector('input[name="p4"]:checked').value,
+        p5: document.querySelector('input[name="p5"]:checked').value,
+    };
+
+    const votosA = Object.values(respuestas).filter(respuesta => respuesta === "A").length;
+    const votosB = Object.values(respuestas).filter(respuesta => respuesta === "B").length;
+
+    if (votosA > votosB) {
+        document.getElementById("resultado").innerHTML = "<h3>Tu candidato para Presidente es Sergio Massa!!!</h3><br><img src='./imagenes/massa.jpg' alt='Massa'></img><br><br><h5>Muchas Gracias por tu tiempo.</h5>";
+    } else {
+        document.getElementById("resultado").innerHTML = "<h3>Tu candidato para Presidente es Javier Milei!!!</h3><br><img src='./imagenes/Milei.jpg' alt='Milei'></img><br><br><h5>Muchas Gracias por tu tiempo.</h5>";
+    }
+}
